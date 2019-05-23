cc9a5dfce40284f7972fc303c61497186fa51409
sighttviewliu <denchance@gmail.com>
2019-05-23 11:35:38 +0800

    title: centos7.4 comment: Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@
 FROM scratch
 ADD centos-7.4.1708-docker.tar.xz /
 
-CMD [ "/bin/bash" ]
+#CMD [ "/bin/bash" ]
+CMD [ "sh", "-c", "/bin/bash" ]
--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+## For testing
 docker run -it --rm --name centos74 centos:7.4 /bin/bash
+
+## For testing
+docker run -it --rm --name centos74 --privileged centos:7.4 /usr/sbin/init
+
+## For testing
+docker -D run -it --rm --name centos74 --privileged centos:7.4 /usr/sbin/init
+
+## For testing
+docker -D run -it --rm --name centos74 --privileged -p 18880:80 centos:7.4 /usr/sbin/init
