d932c992b305997dfff567e22713426d008460df
sighttviewliu <denchance@gmail.com>
2019-05-24 12:43:30 +0800

    title: centos7.4 comment: Dockerfile

--- run.sh
+++ run.sh
@@ -9,3 +9,6 @@ docker -D run -it --rm --name centos74 --privileged centos:7.4 /usr/sbin/init
 
 ## For testing
 docker -D run -it --rm --name centos74 --privileged -p 18880:80 centos:7.4 /usr/sbin/init
+
+## For testing
+docker -D run -it --rm --name centos74 --privileged -p 18880:80 -v $(pwd)/config:/etc/selinux/config centos:7.4 /usr/sbin/init
